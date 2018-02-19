2823a54ea1eebdf52f8d9e613eb5ecf9c8cf838c
Tuan Pham Anh <baabeetaa@gmail.com>
2018-02-20 06:46:03 +0700

    - disable phone verifying - use yandex mail for testing ( should be replaced by smoke email service for production )

--- src/server/sendEmail.js
+++ src/server/sendEmail.js
@@ -1,9 +1,25 @@
-import sendgrid from 'sendgrid';
+// import sendgrid from 'sendgrid';
 import config from 'config';
 
-const sg = sendgrid(config.get('sendgrid.key'));
+const nodemailer = require('nodemailer');
+// const sg = sendgrid(config.get('sendgrid.key'));
 
-export default function sendEmail(template, to, params, from = null) {
+// Use yandex for testing
+const transporter = nodemailer.createTransport({
+    host: 'smtp.yandex.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: config.get('sendgrid.from'),
+        pass: config.get('sendgrid.key')
+    },
+    logger: true,
+    debug: true,
+    disableUrlAccess: false
+});
+
+export default function sendEmail(template, to, params /*, from = null */) {
+    /*******
     if (process.env.NODE_ENV !== 'production') {
         console.log(`mail: to <${to}>, from <${from}>, template ${template} (not sent due to not production env)`);
         return;
@@ -30,5 +46,32 @@ export default function sendEmail(template, to, params, from = null) {
     })
     .catch(error => {
         console.error(`failed to send '${template}' email to '${to}'`, error);
+    });
+
+    */
+
+    /**
+     * Use any mail for testing first, should be replaced by current smoke email service with custom domain eg smoke.io
+     */
+    const email_text = `Enter the link below to confirm your email:
+    http://51.15.217.173/confirm_email/${params.confirmation_code}`;
+
+    const email_html = `Enter the link below to confirm your email:
+    <a href="http://51.15.217.173/confirm_email/${params.confirmation_code}">${params.confirmation_code}</a>`;
+
+    const mailOptions = {
+        from: config.get('sendgrid.from'),
+        to: `${to}`,
+        subject: `SMOKE: ${template}`,
+        text: email_text,
+        html: email_html
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(JSON.stringify(info));
+            return console.log(JSON.stringify(error));
+        }
+        return console.log('Message sent: %s', info.messageId);
     });
 }
