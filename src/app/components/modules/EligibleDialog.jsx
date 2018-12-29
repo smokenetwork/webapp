850e6e65e0af97d012e0266aff0273a41b6f3e67
Jonathan Hunter <theanxiousstoner@gmail.com>
2018-12-29 15:19:28 +0200

    Update EligibleDialogue WIP

--- src/app/components/modules/EligibleDialog.jsx
+++ src/app/components/modules/EligibleDialog.jsx
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import {Link} from 'react-router';
 import {connect} from 'react-redux'
 import user from "../../redux/User";
+import SvgImage from '../elements/SvgImage';
+import tt from 'counterpart';
 
 class EligibleDialog extends Component {
 
@@ -20,14 +22,25 @@ class EligibleDialog extends Component {
 
   render() {
     return (
-      <div>
-        <h4>Are you eligible to visit smoke.io?</h4>
-        I am at least 21 years old or a valid medical marijuana patient and agree to the <Link to='/tos.html'>Terms of
-        Service</Link> and <Link to='/privacy.html'>Privacy Policy</Link>.
-        <hr/>
-        <button type="submit" className="button" onClick={this.readnContinue}>
-          Continue
-        </button>
+      <div className="EligibleWelcome">
+        <div className="text-center">
+          <SvgImage name="smoke" width="300px" className="Header__logo"></SvgImage>
+          <h2>{tt('navigation.intro_tagline')}</h2>
+          <br/>
+          <hr/>
+          <a className="button button--primary EligibleButton" onClick={this.readnContinue} href="/pick_account">
+            <b>{tt('navigation.sign_up')}</b> </a>
+          <a className="button button--primary EligibleButton" onClick={this.readnContinue} href="/login.html">
+            <b>{tt('g.login')}</b> </a>
+          <br />
+          Or <a type="submit" onClick={this.readnContinue}>
+              continue without signing up...
+          </a>
+          <hr />
+          <h4>Are you eligible to visit smoke.io?</h4>
+          By entering, I am at least 21 years old or a valid medical marijuana patient and agree to the <Link to='/tos.html'>Terms of
+          Service</Link> and <Link to='/privacy.html'>Privacy Policy</Link>.
+        </div>
       </div>
     )
   }
