857fb3e005b0283b4c7ebd7aff57c80afd14005f
Jonathan Hunter <theanxiousstoner@gmail.com>
2019-02-22 18:11:04 +0200

    Change Welcome To Sponsor

---
+++ src/app/components/pages/Sponsor.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {Link} from 'react-router';
+
+
+class Sponsors extends React.Component {
+  render() {
+    return (
+      <div>
+
+        <div className="row">
+          <div className="large-12 medium-12 small-12 text-center">
+            <h1 className="title">Our Sponsors</h1>
+            <a className="button">Become A Sponsor</a>
+          </div>
+          <div className="columns large-4 medium-4 small-12">
+            <div className="sponsor-card">
+               <Link to="username"><h2>Display Name/Username</h2></Link>
+               <Link to="username"><img src="https://via.placeholder.com/150" /></Link>
+               <div className="module fade">
+                <p>ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOU</p>
+               </div>
+               <p className="elipsis"><a href="https://settingsurl.com">https://website.com/skhfkshfkshshk</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = {
+  path: 'sponsors',
+  component: Sponsors
+};
