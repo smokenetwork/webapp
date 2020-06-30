1c110630ae0f93031f4791ffe7fc8a38dcd8fa72
indica smoke <grcnode@gmail.com>
2020-07-01 00:41:44 +0100

    Fix Follow Status Mismatch
    https://github.com/steemit/condenser/pull/3091

--- src/app/redux/TransactionSaga.js
+++ src/app/redux/TransactionSaga.js
@@ -44,12 +44,12 @@ const hook = {
   preBroadcast_transfer,
   preBroadcast_vote,
   preBroadcast_account_witness_vote,
-  preBroadcast_custom_json,
   error_vote,
   error_custom_json,
   // error_account_update,
   error_account_witness_vote,
   accepted_comment,
+  accepted_custom_json,
   accepted_delete_comment,
   accepted_vote,
   accepted_account_update,
@@ -94,38 +94,45 @@ function* preBroadcast_account_witness_vote({operation, username}) {
   return operation
 }
 
-function* preBroadcast_custom_json({operation}) {
-  const json = JSON.parse(operation.json)
-  if (operation.id === 'follow') {
-    try {
-      if (json[0] === 'follow') {
-        const {follower, following, what: [action]} = json[1]
-        yield put(g.actions.update({
-          key: ['follow', 'getFollowingAsync', follower],
-          notSet: Map(),
-          updater: m => {
-            //m = m.asMutable()
-            if (action == null) {
-              m = m.update('blog_result', Set(), r => r.delete(following))
-              m = m.update('ignore_result', Set(), r => r.delete(following))
-            } else if (action === 'blog') {
-              m = m.update('blog_result', Set(), r => r.add(following))
-              m = m.update('ignore_result', Set(), r => r.delete(following))
-            } else if (action === 'ignore') {
-              m = m.update('ignore_result', Set(), r => r.add(following))
-              m = m.update('blog_result', Set(), r => r.delete(following))
-            }
-            m = m.set('blog_count', m.get('blog_result', Set()).size)
-            m = m.set('ignore_count', m.get('ignore_result', Set()).size)
-            return m//.asImmutable()
-          }
-        }))
-      }
-    } catch (e) {
-      console.error('TransactionSaga unrecognized follow custom_json format', operation.json);
+function updateFollowState(action, following, state) {
+    if (action == null) {
+        state = state.update('blog_result', Set(), r => r.delete(following));
+        state = state.update('ignore_result', Set(), r => r.delete(following));
+    } else if (action === 'blog') {
+        state = state.update('blog_result', Set(), r => r.add(following));
+        state = state.update('ignore_result', Set(), r => r.delete(following));
+    } else if (action === 'ignore') {
+        state = state.update('ignore_result', Set(), r => r.add(following));
+        state = state.update('blog_result', Set(), r => r.delete(following));
     }
-  }
-  return operation
+    state = state.set('blog_count', state.get('blog_result', Set()).size);
+    state = state.set('ignore_count', state.get('ignore_result', Set()).size);
+    return state;
+}
+
+function* accepted_custom_json({ operation }) {
+    const json = JSON.parse(operation.json);
+    if (operation.id === 'follow') {
+        console.log(operation);
+        try {
+            if (json[0] === 'follow') {
+                const { follower, following, what: [action] } = json[1];
+                yield put(
+                    globalActions.update({
+                        key: ['follow', 'getFollowingAsync', follower],
+                        notSet: Map(),
+                        updater: m => updateFollowState(action, following, m),
+                    })
+                );
+            }
+        } catch (e) {
+            console.error(
+                'TransactionSaga unrecognized follow custom_json format',
+                operation.json
+            );
+        }
+    }
+    return operation;
 }
 
 function* error_account_witness_vote({operation: {account, witness, approve}}) {
