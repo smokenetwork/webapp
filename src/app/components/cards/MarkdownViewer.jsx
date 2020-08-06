96b1b5972cced091d6363ba2ebc7e2b99c576fc8
indica <grcnode@gmail.com>
2020-08-06 14:35:37 +0100

    Add support for embedded dtube videos
    https://github.com/steemit/condenser/pull/3296

--- src/app/components/cards/MarkdownViewer.jsx
+++ src/app/components/cards/MarkdownViewer.jsx
@@ -110,7 +110,7 @@ class MarkdownViewer extends Component {
 
     // HtmlReady inserts ~~~ embed:${id} type ~~~
     for (let section of cleanText.split('~~~ embed:')) {
-      const match = section.match(/^([A-Za-z0-9\?\=\_\-]+) (youtube|vimeo|twitch|spotify|spotifyLarge) ~~~/)
+      const match = section.match(/^([A-Za-z0-9\?\=\_\-\/]+) (youtube|vimeo|twitch|spotify|spotifyLarge|dtube) ~~~/)
       if (match && match.length >= 2) {
         const id = match[1]
         const type = match[2]
@@ -168,6 +168,20 @@ class MarkdownViewer extends Component {
           )
         } else if (type === 'twitch') {
           const url = `https://player.twitch.tv/${id}`;
+          sections.push(
+              <div className="videoWrapper">
+                  <iframe
+                      key={idx++}
+                      src={url}
+                      width={w}
+                      height={h}
+                      frameBorder="0"
+                      allowFullScreen
+                  />
+              </div>
+          );
+        } else if (type === 'dtube') {
+          const url = `https://emb.d.tube/#!/${id}`;
           sections.push(
               <div className="videoWrapper">
                   <iframe
